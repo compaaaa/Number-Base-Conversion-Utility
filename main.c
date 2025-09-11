ec4aaa62f2954e91062dece1ccf21cbbd14c8203
Haziel <113366708+compaaaa@users.noreply.github.com>
2025-09-11 16:08:16 -0400

    Created test in main

--- main.c
+++ main.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 #include <stdint.h>
-#include <stdio.h>
 
 void div_convert(uint32_t n, int base, char *out);
 void sub_convert(uint32_t n, int base, char *out);
+
+int main(){
+    char result[65];
+
+    div_convert(156, 8, result);
+    printf("div_convert: 156 converted to base 8 is: %s\n", result);
+    div_convert(255, 16, result);
+    printf("div_convert: 255 converted to base 16 is: %s\n", result);
+    div_convert(104, 5, result);
+    printf("div_convert: 104 converted to base 5 is: %s\n", result);
+
+    sub_convert(190, 3, result);
+    printf("sub_convert: 190 converted to base 3 is: %s\n", result);
+    sub_convert(156, 8, result);
+    printf("sub_convert: 156 converted to base 8 is: %s\n", result);
+    sub_convert(104, 5, result);
+    printf("sub_convert: 104 converted to base 5 is: %s\n", result);
+    return 0;
+}
